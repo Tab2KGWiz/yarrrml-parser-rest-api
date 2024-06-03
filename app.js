af1619d4bdead4d2b53aba3405ff3e6764ab545f
ZcR-01 <zihan1048@live.com>
2024-06-03 11:06:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const tools = require('@rmlio/yarrrml-parser/lib/tools');
+const convertYAMLtoRML = require('@rmlio/yarrrml-parser/lib/rml-generator');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
+
+app.post('/yarrrml', upload.single('yamlFile') , (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+
+  fs.readFile(req.file.path, 'utf8', async (err, yamlContent) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to read the file' });
+    }
+
+    //const y2r = new yarrrml();
+    const y2r = new convertYAMLtoRML();
+
+    const triples = y2r.convert(yamlContent);
+    //const jsonldObj = JSON.parse(JSON.stringify(jsonld));
+
+    const canonicalTurtle = await tools.logCanonical(triples, y2r.getPrefixes(), y2r.getBaseIRI());
+
+    fs.unlink(req.file.path, (err) => {
+      if (err) {
+        console.error('Failed to delete the file:', req.file.path);
+      }
+    });
+
+    if (y2r.getLogger().has('error')) {
+      const logs = y2r.getLogger().getAll();
+      return res.status(400).json(logs);
+    }
+
+    return res.status(200).json(canonicalTurtle);
+  });
+});
